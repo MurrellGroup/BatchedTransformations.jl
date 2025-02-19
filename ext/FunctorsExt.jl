082e4363db6e947ca2a70a1af6422ec3d4dee841
Anton Oresten <antonoresten@gmail.com>
2025-02-19 15:19:21 +0100

    Fix Linear functor

--- ext/FunctorsExt.jl
+++ ext/FunctorsExt.jl
@@ -1,12 +1,19 @@
 module FunctorsExt
 
 using BatchedTransformations
-using Functors: @functor
+import Functors: @functor, functor
 
 @functor Inverse
 @functor Composed
-@functor Linear
 @functor Translation
 @functor Affine
+
+# See:
+# https://github.com/FluxML/Functors.jl/issues/40
+# https://github.com/FluxML/Functors.jl/issues/6#issuecomment-744895426
+function functor(::Type{<:Linear{M}}, x) where M
+    reconstruct_linear(xs) = Linear{M}(xs.values)
+    return (; values = x.values), reconstruct_linear
+end
 
 end
